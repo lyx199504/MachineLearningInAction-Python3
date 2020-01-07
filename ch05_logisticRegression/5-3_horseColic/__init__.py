4733c2f29d8edfb1d008196bad438bc49f6d6b9b
lyx <765020742@qq.com>
2020-01-07 22:06:15 +0800

    新增数据

---
+++ ch05_logisticRegression/5-3_horseColic/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/1/7 22:02
+# @Author : LYX-夜光
---
+++ ch05_logisticRegression/5-3_horseColic/horseColic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/1/7 22:03
+# @Author : LYX-夜光
+
+from ch05_logisticRegression import logistic
+
