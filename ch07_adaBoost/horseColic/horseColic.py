e41ed4891775ec49390c4ec591a920cc3ccf0f75
lyx <765020742@qq.com>
2020-01-13 22:32:04 +0800

    新增数据样例

---
+++ ch07_adaBoost/horseColic/horseColic.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/1/13 10:14
+# @Author : LYX-夜光
+
+from ch07_adaBoost import adaBoost
+import numpy as np
+
+# 初始化数据
+def loadDataSet(fileName):
+    dataMat, labelMat = [], []
+    readFile = open(fileName)
+    numFeat = len(readFile.readline().split('\t'))
+    for line in readFile.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat - 1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+
+if __name__ == "__main__":
+    dataArr, labelArr = loadDataSet('horseColicTraining2.txt')
+    classifierArray = adaBoost.adaBoostTrainDS(dataArr, labelArr, 50)
+    testData, testLabel = loadDataSet('horseColicTest2.txt')
+    prediction10 = adaBoost.adaClassify(testData, classifierArray)
+    m = np.shape(testData)[0]
+    errArr = np.mat(np.ones((m, 1)))
+    errorSum = errArr[prediction10 != np.mat(testLabel).T].sum()
+    print("error rate is ", errorSum/m)
